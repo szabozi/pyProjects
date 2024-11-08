74c11cd4358268592400de492515b98188974b0f
szabozi <48012968+szabozi@users.noreply.github.com>
2024-11-08 19:44:33 +0200

    Delete Next_to_Read/next_to_read_2_with_import directory

--- Next_to_Read/next_to_read_2_with_import/next_to_read_2.py
+++
@@ -1,7 +0,0 @@
-import bookLists
-import random
-
-series = bookLists.bookSeriesList
-
-bookSelected = random.randint(1, len(series))
-print("Next book series to read is number {} - {} ".format(bookSelected+1, series[bookSelected]))
