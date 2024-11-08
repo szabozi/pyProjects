5f2e1893a1e9525a7183b1d3a3a76e3915a39017
szabozi <48012968+szabozi@users.noreply.github.com>
2024-11-08 19:44:44 +0200

    Delete Next_to_Read/next_to_read_3_with_txt directory

--- Next_to_Read/next_to_read_3_with_txt/next_to_read_3.py
+++
@@ -1,7 +0,0 @@
-import random
-
-bookLists = open('bookLists.txt').read().splitlines()
-listCount = len(bookLists)
-bookSelected = random.randint(1, listCount)
-
-print("Next book series to read is number {} - {} ".format(bookSelected+1, bookLists[bookSelected]))
